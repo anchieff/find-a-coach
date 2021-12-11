fbef44ded614af51f4f06cd015289db64a93998b
anchieff <info@annanekrasova.ru>
2021-12-11 18:37:16 +0400

    uninstall tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import router from './routes'
 import App from './App.vue'
 import './index.css'
+import './assets/tailwind.css'
 
 const app = createApp(App)
 app.use(router)
