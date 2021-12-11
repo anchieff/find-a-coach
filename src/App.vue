f297a595406870d6bd4e2919b716c684eba8ae85
anchieff <info@annanekrasova.ru>
2021-12-11 18:31:44 +0400

    feat: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,26 @@
 <template>
+    <the-header></the-header>
     <router-view></router-view>
 </template>
+
+<script>
+import TheHeader from './components/layout/TheHeader.vue'
+export default {
+    components: {
+        TheHeader
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat', sans-serif;
+}
+</style>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <nav>
+            <h1>
+                <router-link to="/">Find a Coach</router-link>
+            </h1>
+            <ul class="list-none">
+                <li class="text-3xl font-bold underline">
+                    <router-link to="/coaches">All Coaches</router-link>
+                </li>
+                <li>
+                    <router-link to="/requests">Requests</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
